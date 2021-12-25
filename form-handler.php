5c137a10ae931c8d4175eb6df4b35c390b3d163b
srinidhiVN <srinidhivn93@gamil.com>
2021-12-25 14:14:19 +0530

    first commit

---
+++ form-handler.php
@@ -0,0 +1,27 @@
+<?php>
+
+$name=$_POST['Name'];
+$visitor_email=$_POST['Email'];
+$subject=$_POST['Subject'];
+$message=$_POST['Message'];
+
+$email_from='nam@gmail.com';
+
+$email_subject='New Form Submission';
+
+$email_body="User Name: $name.\n"
+"User Email: $visitor_email.\n"
+"Subject: $subject.\n"
+"User Message: $message.\n";
+
+$to='nam@gmail.com';
+
+$headers="From $email_from \r\n";
+
+$headers .="Reply to $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+?>
